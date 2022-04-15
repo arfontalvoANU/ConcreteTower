acac92dbcfa4f5de9801f9bc668132337cc088fd
arfontalvo <armando.fontalvo@anu.edu.au>
2022-04-15 10:14:49 +1000

    Setting IsSodium option to False for nitrate salt receiver

--- example_abengoa.py
+++ example_abengoa.py
@@ -7,6 +7,9 @@ warnings.filterwarnings('ignore', 'The iteration is not making good progress')
 D_receiver = 20.                                                      # [m]
 H_receiver = 25.                                                      # [m]
 Q_rec_out = 936                                                       # [MWth]
+T_in = 290.                                                           # [deg C]
+T_out = 566.                                                          # [deg C]
+IsSodium = False                                                      # Set to False to use nitrate salt
 Tower_height = 268.                                                   # [m]
 Section_height = 5.                                                   # [m]
 Thickness_bottom = 0.566                                              # [m]
@@ -15,6 +18,6 @@ Diameter_bottom = 30.4                                                # [m]
 Diameter_top = 20.2                                                   # [m]
 verbose = True                                                        # True to print intermediate output
 
-Receiver_weight = scaling_w_receiver(D_receiver, H_receiver, Q_rec_out)
+Receiver_weight = scaling_w_receiver(D_receiver, H_receiver, Q_rec_out, T_in, T_out, IsSodium)
 
 C_tower,sf_min,res_min = tower(Tower_height,Section_height,Receiver_weight,Thickness_bottom,Thickness_top,Diameter_bottom,Diameter_top,verbose)
